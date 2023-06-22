8d0383c5d790a223fd1c2d35b6834f37536eb43e
Robin s1997 <306206594@qq.com>
2023-06-22 11:48:47 +0800

    测试

--- src/main/java/com.bb.Utils/StringUtils.java
+++ src/main/java/com.bb.Utils/StringUtils.java
@@ -1,14 +1,16 @@
 package com.bb.Utils;
 
 public class StringUtils {
-    public static boolean isEmpty(String str){
+    public static boolean isEmpty(String str) {
 
-            return str == null || "".equals(str);
+        return str == null || "".equals(str);
 
     }
 
-    public static boolean isNotEmpty(String str){
-
+    public static boolean isNotEmpty(String str) {
+        if (isEmpty(str)) {
+            System.out.println("新加的push");
+        }
         return !isEmpty(str);
 
     }
